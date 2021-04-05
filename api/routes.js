99753eeb68d17bf4579741a32ae094914ed45415
fsrocha-dev <contato.frankrocha@gmail.com>
2021-04-05 20:27:59 -0300

    Created routes file

---
+++ api/routes.js
@@ -0,0 +1,8 @@
+module.exports = function(app) {
+    app.get('/about', function(req, res) {
+    //
+    });
+    app.get('/get-cep-info', function(req, res) {
+    //
+    });
+  };
--- service.js
+++ service.js
@@ -2,6 +2,8 @@ const express = require("express")
 
 const app = express()
 
+require('./api/routes')(app);
+
 const PORT  = process.env.PORT || 3000
 
 app.listen(PORT, () => { console.log(`Service running on port: ${PORT}`) })
